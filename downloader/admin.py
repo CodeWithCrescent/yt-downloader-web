a678e45b0948ff1f5ca223eaa3e0db081b146474
Crescent Sambila <crescent.sambila@gmail.com>
2026-05-04 15:15:46 +0300

    admin dashboard

--- downloader/admin.py
+++ downloader/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import VideoDownload, VideoFormat
 
-# Register your models here.
+@admin.register(VideoDownload)
+class VideoDownloadAdmin(admin.ModelAdmin):
+    list_display = ['id', 'video_title', 'user', 'status', 'created_at']
+    list_filter = ['status', 'created_at']
+    search_fields = ['video_title', 'video_url']
+    readonly_fields = ['created_at', 'completed_at']
+
+@admin.register(VideoFormat)
+class VideoFormatAdmin(admin.ModelAdmin):
+    list_display = ['id', 'download_instance', 'resolution', 'filesize']
+    list_filter = ['resolution']
+    
