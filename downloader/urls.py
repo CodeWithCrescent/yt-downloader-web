e6766cba07e00b561dbfb1255043c52bde57c6a1
Crescent Sambila <crescent.sambila@gmail.com>
2026-05-04 19:58:25 +0300

    Updated urls

--- downloader/urls.py
+++ downloader/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('get-video-info/', views.get_video_info, name='get_video_info'),
-    path('check-video-status/<str:task_id>/', views.check_video_status, name='check_video_status'),
+    path('get-media-info/', views.get_media_info, name='get_media_info'),
+    path('check-media-status/<str:task_id>/', views.check_media_status, name='check_media_status'),
     path('start-download/', views.start_download, name='start_download'),
     path('check-download-status/<str:task_id>/', views.check_download_status, name='check_download_status'),
+    path('download-file/<int:download_id>/', views.download_completed_file, name='download_completed_file'),
     path('my-downloads/', views.my_downloads, name='my_downloads'),
-    path('download-file/<int:download_id>/', views.download_file, name='download_file'),
+    path('supported-platforms/', views.supported_platforms, name='supported_platforms'),
 ]
