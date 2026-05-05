0434c8203c6bb9ffa0e545d4347a5aefb6d4d470
Crescent Sambila <crescent.sambila@gmail.com>
2026-05-05 12:15:14 +0300

    Added support for dynamic Celery beat schedule configuration from Django settings

--- core/celery.py
+++ core/celery.py
@@ -1,8 +1,12 @@
 import os
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 app = Celery('core')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+if getattr(settings, 'CELERY_BEAT_SCHEDULE', None):
+    app.conf.beat_schedule = settings.CELERY_BEAT_SCHEDULE
