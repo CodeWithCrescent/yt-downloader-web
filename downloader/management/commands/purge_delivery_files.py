37dc820926b4e570e1860fdee3d3136ad282d3d0
Crescent Sambila <crescent.sambila@gmail.com>
2026-07-21 14:30:00 +0300

    Update the app

--- downloader/management/commands/purge_delivery_files.py
+++ downloader/management/commands/purge_delivery_files.py
@@ -2,7 +2,6 @@ from django.core.management.base import BaseCommand
 
 from downloader.delivery import run_purge_expired_deliveries
 
-
 class Command(BaseCommand):
     help = 'Delete packaged files older than DELIVERY_TTL_MINUTES and clear delivery tokens.'
 
